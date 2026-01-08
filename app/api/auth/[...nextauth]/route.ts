8dad37ca4225af9b79f9dad9b55b89197258cc6a
Taka-75 <itami456@yahoo.co.jp>
2026-01-08 10:22:49 +0900

    update

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,11 @@
 import { nextAuthOptions } from "@/app/lib/next-auth/options";
-import NextAuth from "next-auth";
-const handler = NextAuth(nextAuthOptions);
 
-// https://next-auth.js.org/configuration/initialization#route-handlers-app
-export { handler as GET, handler as POST };
+import NextAuth from "next-auth"
+
+
+const handler = NextAuth(nextAuthOptions)
+
+export { handler as GET, handler as POST }
+
+// これで直らない場合に有効（Edgeで落ちる回避）
+export const runtime = "nodejs"
